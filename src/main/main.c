58b0ba5c8dbc6f1e1b2f923bc406066a893c6a4b
mathroy0310 <mathroy0310@gmail.com>
2024-08-22 10:51:19 -0400

    added free to fix leaks

--- src/main/main.c
+++ src/main/main.c
@@ -6,18 +6,28 @@
 /*   By: maroy <maroy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 01:35:21 by maroy             #+#    #+#             */
-/*   Updated: 2024/08/21 01:49:27 by maroy            ###   ########.fr       */
+/*   Updated: 2024/08/22 10:48:53 by maroy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
 
+void free_command(Command *cmd) {
+	for (int i = 0; i < cmd->size; i++) {
+		free(cmd->args[i].content);
+	}
+	free(cmd->args);
+}
+
 int main(int ac, char **av) {
 	Command cmd = get_cmd(ac, av);
 
 	if (fatal_error(&cmd))
+	{
+		free_command(&cmd);
 		return 2;
+	}
 
 	for (int i = 0; i < cmd.size; i++) {	
 		if (cmd.args[i].type & OPTION)
@@ -32,11 +42,13 @@ int main(int ac, char **av) {
 	if (cmd.flags & list)
 		ft_printf("\t-List\n");
 	if (cmd.flags & all)
-		ft_printf("\t-All\n");	
+		ft_printf("\t	-All\n");	
 	if (cmd.flags & reverse)
 		ft_printf("\t-Reverse\n");	
 	if (cmd.flags & time)
 		ft_printf("\t-Time\n");	
 	if (cmd.flags & help)
 		ft_printf("\t-Help\n");	
+
+	free_command(&cmd);
 }
